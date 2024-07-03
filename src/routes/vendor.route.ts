51b4450fc2fdcf60177b96aac669bf376b09f78f
moronjie <njiemoro2@gmail.com>
2024-07-03 00:08:14 +0000

    vendor routes

--- src/routes/vendor.route.ts
+++ src/routes/vendor.route.ts
@@ -1,11 +1,23 @@
-import { Router } from "express";
+import { NextFunction, Router, Response } from "express";
+import { authUser } from "../middleware/jwt.auth";
+import { createVendor, deleteVendor, getAllVendors, updateVendor } from "../controllers/vendor.controller";
+import { getASingleUser } from "../controllers/user.controler";
 
 const router = Router()
 
 //create vendor
+router.post("/", authUser, createVendor)
+
 //get vendors 
+router.get("/", getAllVendors)
+
 //get single vendor
+router.get("/:id", getASingleUser)
+
 //update vendor
+router.put("/:id", authUser, updateVendor)
+
 //delete vendor
+router.delete("/:id", authUser, deleteVendor)
 
 export default router 
